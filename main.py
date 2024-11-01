42adbd855e9b3ee8da4a0c6c046ced558ea875d2
aahm2221 <aisha.ahmed1@yahoo.com>
2024-11-01 11:27:23 -0500

    create print report for word and char info

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ def main():
     book_path = "books/frankenstein.txt"
     file_contents = get_text(book_path)
     word_count = word_counter(file_contents)
-    print(word_count)
     char_count = char_counter(file_contents)
-    for char in char_count:
-        print(f"{char}: {char_count[char]}")
+    char_info = sort_char(char_count)
+    
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document.\n")
+    for info in char_info:
+        print(f"The '{info["name"]}' character was found {info["num"]} times.")
+    print("--- End Report ---")
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_char(char_dict):
+    sorted = []
+    for char in char_dict:
+        sorted.append({"name": char, "num": char_dict[char]})
+    sorted.sort(reverse=True, key=sort_on)
+    return sorted
+    
 
 def char_counter(text):
     chars = {}
@@ -13,7 +28,7 @@ def char_counter(text):
         if char.lower() in chars:
             chars[char.lower()] += 1
         else:
-            if char != "\n":
+            if char.isalpha():
                 chars[char.lower()] = 1
     return chars
 
