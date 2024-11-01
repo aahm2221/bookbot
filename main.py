3aca3c6a6a0d66ebf08264c83a6a39941e388a36
aahm2221 <aisha.ahmed1@yahoo.com>
2024-11-01 10:50:04 -0500

    change functionality to word count

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        word_count = file_contents.split()
+        print(len(word_count))
 
 main()
