e6d547cd5462fdbd63dce84acb43d41cd249e79a
aahm2221 <aisha.ahmed1@yahoo.com>
2024-11-01 10:56:44 -0500

    clean up counting functionality

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        word_count = file_contents.split()
-        print(len(word_count))
+    book_path = "books/frankenstein.txt"
+    file_contents = get_text(book_path)
+    word_count = word_counter(file_contents)
+    print(word_count)
+
+def word_counter(text):
+    return len(text.split())
+
+
+def get_text(path):
+    with open(path) as f:
+       return f.read()
 
 main()
